77e33194a7183dfb08210aece361521eca384e06
Bruno de Almeda Otero <b.otero@aluno.ifsp.edu.br>
2024-12-23 17:30:29 +0000

    first commit

--- app/main/views.py
+++ app/main/views.py
@@ -1,18 +1,22 @@
 from flask import render_template, session, redirect, url_for, current_app
 from .. import db
-from ..models import User
+from ..models import User, Role
 from ..email import send_email, send_simple_message
 from . import main
-from .forms import NameForm
+from .forms import NameAndEmailForm
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    form = NameAndEmailForm()
+    user_all = User.query.all()
+    print(user_all)
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(username=form.name.data)
+            user_role = Role.query.filter_by(name='User').first()
+            user = User(username=form.name.data, role=user_role)
+            userEmail = form.email.data
             db.session.add(user)
             db.session.commit()
             session['known'] = False
@@ -20,7 +24,9 @@ def index():
             if current_app.config['FLASKY_ADMIN']:
                 #send_email(current_app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
                 print('Enviando mensagem...', flush=True)
-                send_simple_message([current_app.config['FLASKY_ADMIN'], "flaskaulasweb@zohomail.com"], 'Novo usuário', form.name.data)
+                send_simple_message([current_app.config['FLASKY_ADMIN'], "flaskaulasweb@zohomail.com"
+                                                                            # , userEmail
+                ], 'Novo usuário', form.name.data)
                 print('Mensagem enviada...', flush=True)
         else:
             session['known'] = True
@@ -28,4 +34,5 @@ def index():
         return redirect(url_for('.index'))
     return render_template('index.html',
                            form=form, name=session.get('name'),
-                           known=session.get('known', False))
+                           known=session.get('known', False),
+                           user_all=user_all)
